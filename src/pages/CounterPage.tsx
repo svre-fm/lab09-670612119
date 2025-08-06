f703b523990184f8d2f7bbdd05025231ae2d2387
Svre_fm <Firmoioi96@gmail.com>
2025-08-06 15:19:34 +0700

    complete

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function CounterPage() {
+  const[count,setCount] = useState(0);
+  const addcount = () => {
+    setCount( count + 1);
+  }
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
-      {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      {}
+      <p>Counter: {count} </p>
+      <button onClick = {addcount}>Increase</button>
     </div>
   );
 }
